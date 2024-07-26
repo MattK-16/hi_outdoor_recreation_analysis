833142a97d864d9135124d652719a48ef17e7928
MattK-16 <116367795+MattK-16@users.noreply.github.com>
2024-07-26 11:24:42 -0400

    Synchronization and trackId column fix
    Added the files for github action configuration. Updated trackId identification.

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -11,16 +11,16 @@ print("Conducting unit tests. ")
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        os.environ['APP_ARTIFACTS_DIR'] = os.path.join(ROOT_DIR, 'tests/resources/output')
+        os.environ['APP_ARTIFACTS_DIR'] = os.path.join('C:/Users/matth/hi_outdoor_recreation_analysis', 'tests/resources/output')
         self.sut = App(moveapps_io=MoveAppsIo())  
 
     #Checks the input file compared to the output after it was run
     def test_app_returns_input(self):
         # prepare
-        expected = pd.read_pickle("./resources/samples/martesAllNY.pickle")
+        expected = pd.read_pickle("./resources/samples/martesJul24.pickle")
 
         #config: dict = {}
-        config: dict = {"keys": "Key-Pair-Id=APKAIDPUN4QMG7VUQPSA&Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHBzOi8vaGVhdG1hcC1leHRlcm5hbC0qLnN0cmF2YS5jb20vKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTY4NTcyOTQyOH0sIkRhdGVHcmVhdGVyVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjg0NTA1NDI4fX19XX0_&Signature=hJFIxHJuYXQW0XiqaeBCqy6dKU79BYRRNKr-y6bVgYBaO1Z9horGitL2T4fBi7nCQv3Q89VRhQ0TSYrQg4lsIjVntz7YGCFE3d8O3VFrB3KaI7ghY-NIqxF~S8SN5hf7uM8F0bOjTiqTcj02dLkcNXYtJ3Rt7gjDGklqEInengOIQACYxcbKdT3AUwKb5LfcUDUHRuo-wp49lRoKK0Gbcq55i~4uCU4WjO3K2M9a-PkO465bY7qNii2JI~nu4qezCgogoeaQaVAdMM46ucSv5cmN3CdEoc4sUPKcWR8FCxNtbbjmFg7w8AT1~v6XzU0KecKjMdZG8athHblHo7tcow__"}
+        config: dict = {"keys": "Key-Pair-Id=APKAIDPUN4QMG7VUQPSA&Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHBzOi8vaGVhdG1hcC1leHRlcm5hbC0qLnN0cmF2YS5jb20vKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTcyMjc5ODQyOX0sIkRhdGVHcmVhdGVyVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzIxNTc0NDI5fX19XX0_&Signature=iC1sIRxZz3Azaalz~0wPCCzxmXOSEH3hBpfMDOqucqZl7QyAA9UreV74YBQCJJ~fDeLAAqFBpcHb0UTdo50rxXfiWw5S0XB~Hbtbk9DOm5OzpjtqlgDa9ro8tmWxZx~QJSvhXB1YuawWyBc0seeOsZMBlpn30IVCeLaN-QwrRRnGs15irxLubqGwuk7WMdyIgVnYnMQ3qdB-eWpSZ1iez11taiI5TL~TBdgsZ3pCVRa4RKO4kVqj859o406NnKZLgnslz--lI9204wquMk9i8NXDMtfBfs2Qk8eXj-afUAUhBIFrlUnxkkAmDhAHP2kJZZX83fPMBW-scb42QMPwCg__"}
 
         # execute
         actual = self.sut.execute(data=expected, config=config)
